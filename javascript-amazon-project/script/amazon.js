ebdc2392298c7831a678d5ead10620c0a1656efb
shivapathak-code <shivapathak766@gmail.com>
2026-09-11 10:50:38 +0530

    update the chekout.js file use features

--- javascript-amazon-project/script/amazon.js
+++ javascript-amazon-project/script/amazon.js
@@ -1,5 +1,6 @@
 import {cart , addtocart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formateCurrency } from './utils/money.js';
 let productHTMl = '';
 products.forEach((product)=>{
  productHTMl +=  `
@@ -22,7 +23,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formateCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
---
+++ javascript-amazon-project/script/utils/money.js
@@ -0,0 +1,4 @@
+export function formateCurrency(priceCents)
+{
+  return (priceCents/100).toFixed(2);
+}
